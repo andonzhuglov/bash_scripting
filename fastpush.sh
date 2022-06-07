d636da3c5d444094916d5f7c3be664d4eae8633c
Mladen Uzunov <mladen.uzunov@digitaspixelpark.com>
2022-06-07 14:21:40 +0300

    asd

--- fastpush.sh
+++ fastpush.sh
@@ -42,7 +42,7 @@ function pushChanges {
     read -p "Commit message: " commitMessage
     git commit -m "$commitMessage"
     #git push origin $remoteBranch
-    if "$?" ; then print "Pushed to $remoteBranch" || error "Failed to push to $remoteBranch"; fi
+    if [[ "${?}" -eq 0 ]] ; then print "Pushed to $remoteBranch" || error "Failed to push to $remoteBranch"; fi
 }
 
 addFiles
